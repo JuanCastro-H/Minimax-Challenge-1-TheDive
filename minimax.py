3c3a9b2c95685cb3ccfb3915a795e6a8a5b3faf1
Juan Castro <juamcastroh@gmail.com>
2026-04-29 22:39:24 -0300

    feat: agregar logica para implementar al gato el raton y la cueva del raton segun la eleccion del usuario (aleatorio,, esquinas opuestas, esquina y medio)

--- minimax.py
+++ minimax.py
@@ -1,3 +1,6 @@
+# IMPORTACIONES DE LIBRERIAS
+import random # Libreria basica de python para generar numeros aleatorios con un algoritmo
+
 #---------------------------------------------
 # IMPRESION DEL TABLERO DE JUEGO
 #---------------------------------------------
@@ -56,10 +59,78 @@ if __name__ == "__main__":
     for fila in range(filas):
         #      ( Y  ; X )
         tablero[fila][0]            = "#"
-        tablero[fila][columnas - 1] = "#"  
-    
+        tablero[fila][columnas - 1] = "#" 
 
-        # INICIO DEL JUEGO
 
-        print("TABLERO INICIAL")
-        imprimir_tablero(tablero) # Llamamos/Inciamos la funcion para mostrar el tablero en la consola
+
+    # MENU DE LAS POSICIONES DE LOS JUGADORES
+
+    print("\nElige las posiciones del Gato 🐱 y al Ratón 🐭:")
+    print("1. Raton en esquina y Gato en el centro")
+    print("2. Raton en esquina y Gato en esquina opuesta")
+    print("3. Posiciones aleatorias")
+
+    posiciones_jugadores = int(input("Opcion: ")) # Tomamos la opcion que eligio el usuario
+
+
+    # 1 Posiciones: Esquina y centro (pacman)
+    if posiciones_jugadores == 1:
+        posicion_raton = (1, 1) # Raton esquina izquierda
+        posicion_gato  = (filas // 2, columnas //2 ) # Gato en el centro 
+                        #(Divide a la mitad filas y columnas)
+        cueva = (filas - 2, columnas - 2) # la cueva se genera en la esquina opuesta
+        tablero[cueva[0]][cueva[1]] = "🏠"
+
+    # 2 Posiciones: Esquinas opuestas
+    elif posiciones_jugadores == 2:
+        posicion_raton = (1, 1)# Raton esquina izquierda
+        posicion_gato  = (filas - 2, columnas - 2) # Gato ezquina opuesta
+        cueva = (1, columnas - 2) # Genera la cueva cerca del gato
+        tablero[cueva[0]][cueva[1]] = "🏠"
+
+    # 3 Posiciones: Aleatorias
+    elif posiciones_jugadores == 3:
+
+        # Creacion de la posicion aleatoria del raton
+        while True: # Bucle infinito que crea una posicion random para el raton y verifica si es valida
+            posicion_raton = (random.randint(1, filas - 2), random.randint(1, columnas - 2))
+            if tablero[posicion_raton[0]][posicion_raton[1]] == ".":
+                break
+
+
+        # Creacion de la posicion aleatoria del gato
+        while True: #Bucle infinito que crea una posicion random y verifica si es valida y diferente al raton
+            posicion_gato = (random.randint(1, filas - 2), random.randint(1, columnas - 2))
+            if tablero[posicion_gato[0]][posicion_gato[1]] == "." and posicion_gato != posicion_raton:
+                break
+
+        # Creacion de la posicion aleatoria de la cueva 
+        while True: #Bucle infinito que crea una posicion random para la cueva y verifica si es valida y diferente al raton y gato
+            cueva = (random.randint(1, filas - 2), random.randint(1, columnas - 2))
+            if tablero[cueva[0]][cueva[1]] == "." and cueva != posicion_gato and cueva != posicion_raton:
+                break
+        
+        tablero[cueva[0]][cueva[1]] = "🏠" # Pone la cueva y su simbolo en la posicion que le corresponda en el mapa
+
+
+    # Eleccion no valida
+    else: # Se eligio una opcion incorrecta, por tanto se elige una fija por defecto
+        print("Opcion invalida, posicion de gato en el centro por defecto")
+        posicion_raton = (1, 1) # Raton esquina izquierda
+        posicion_gato  = (filas // 2, columnas //2 ) # Gato en el centro 
+        cueva = (filas - 2, columnas - 2)
+        tablero[cueva[0]][cueva[1]] = "🏠"
+
+    # INTRODUCION DEL GATO Y RATON AL TABLERO
+
+
+            # Fila Y            # Columna X
+    tablero[posicion_raton[0]] [posicion_raton[1]] = "🐭"
+    tablero[posicion_gato[0] ] [posicion_gato[1]]  = "🐱"
+    # Reemplazan la celda vacia (representada con ".") con el simbolo del jugador
+
+
+    # INICIO DEL JUEGO
+
+    print("TABLERO INICIAL")
+    imprimir_tablero(tablero) # Llamamos/Inciamos la funcion para mostrar el tablero en la consola
